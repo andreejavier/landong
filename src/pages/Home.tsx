cb571edbe1e4504ca1b6697cb74c813740272008
hexedhorizon <dev.clfm5t12@gmail.com>
2024-05-10 20:45:17 +0800

    Added navbar and initial theme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonLabel, IonMenuButton, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import { IonReactRouter } from '@ionic/react-router';
+import { playCircle, radio, library, search } from 'ionicons/icons';
+import { Redirect, Route } from 'react-router';
+
+
+
+import HomePage from './tabs/MapPage';
+import RadioPage from './tabs/RadioPage';
+import LibraryPage from './tabs/LibraryPage';
 
 const Page1: React.FC = () => {
   return (
@@ -24,7 +33,44 @@ const Page1: React.FC = () => {
           Details
         </IonButton>
     
-    
+        <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Redirect exact path="/" to="/home" />
+          {/*
+          Use the render method to reduce the number of renders your component will have due to a route change.
+
+          Use the component prop when your component depends on the RouterComponentProps passed in automatically.
+        */}
+          <Route path="/home" render={() => <HomePage />} exact={true} />
+          <Route path="/radio" render={() => <RadioPage />} exact={true} />
+          <Route path="/library" render={() => <LibraryPage />} exact={true} />
+
+        </IonRouterOutlet>
+
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={playCircle} />
+            <IonLabel>Listen now</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="radio" href="/radio">
+            <IonIcon icon={radio} />
+            <IonLabel>Radio</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="library" href="/library">
+            <IonIcon icon={library} />
+            <IonLabel>Library</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="search" href="/search">
+            <IonIcon icon={search} />
+            <IonLabel>Search</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
       </IonContent>
     </IonPage>
   );
---
+++ src/pages/tabs/LibraryPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+
+const LibraryPage = () => (
+  <>
+   
+    <IonContent>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+        }}
+      >
+        Library content
+      </div>
+    </IonContent>
+  </>
+);
+
+export default LibraryPage;
